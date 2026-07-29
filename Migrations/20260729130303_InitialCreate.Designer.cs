469a719c0212322eb3adad53318b39d1cd173b5a
rishi <rishikhosla@hotmail.com>
2026-07-29 14:10:57 +0100

    updated connection string in appsettings.json

--- Migrations/20260729130303_InitialCreate.Designer.cs
+++ Migrations/20260729130303_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FiveAsideTournaments.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260729105932_InitialCreate")]
+    [Migration("20260729130303_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
--- obj/Debug/net8.0/FiveAsideTournaments.AssemblyInfo.cs
+++ obj/Debug/net8.0/FiveAsideTournaments.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FiveAsideTournaments")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a8e1eb4f2a01a194d1d75ada562fc528d6016a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d4e8c1a47dbaa91fa0e347b06604f8f52c1092")]
 [assembly: System.Reflection.AssemblyProductAttribute("FiveAsideTournaments")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FiveAsideTournaments")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
